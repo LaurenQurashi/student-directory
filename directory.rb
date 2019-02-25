ad9f96bcddfefbf7e3a4d335a242c73de9c9d2a9
Lauren <laurenqurashi@me.com>
2019-02-25 22:18:08 +0000

    Using .strip instead of .chomp for removal of whitespace

--- directory.rb
+++ directory.rb
@@ -5,7 +5,7 @@ def input_students
   # create an empty array
   students = []
   # get the first name
-  name = gets.chomp.capitalize
+  name = gets.strip
   # while the name is not empty, repeat this code
   while !name.empty? do
     # get and add the student's hobbies and birthplace to the hash.
