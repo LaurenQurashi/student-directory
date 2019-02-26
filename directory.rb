f7b72245fdd2bde2fa7e36ce20534110f8b855fb
Lauren <laurenqurashi@me.com>
2019-02-26 21:44:20 +0000

    Allows student files to be loaded on startup if available

--- directory.rb
+++ directory.rb
@@ -1,11 +1,23 @@
 
 @students = []
 
+def try_load_students
+  filename = ARGV.first # first argument from the command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exists?(filename) # if it exists
+    load_students(filename)
+     puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit # quit the program
+  end
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # get the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
@@ -19,7 +31,7 @@ end
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -49,10 +61,11 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
-  name, cohort = line.chomp.split(',')
+    name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
@@ -89,4 +102,5 @@ end
 def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
+try_load_students
 interactive_menu
